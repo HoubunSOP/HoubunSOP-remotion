c92333f5447b1c5a5f1de342fcd35e7720f586fa
misaka10843 <misaka10843@outlook.jp>
2023-01-23 20:57:12 +0800

    🧪测试figma的联动性

--- src/Title.tsx
+++ src/Title.tsx
@@ -9,11 +9,15 @@ export const Title: React.FC = () => {
 		extrapolateRight: 'clamp',
 	});
 	return (
-		<div
-			style={{opacity}}
-			className="text-gray-700 text-5xl font-bold leading-relaxed"
-		>
-			Welcome to Remotion with Tailwind
-		</div>
+<div style={{opacity}} className="text-gray-700 text-5xl font-bold leading-relaxed">
+	<div className="w-[811px] h-[327px]">
+		<p className="absolute left-[169px] top-[392px] text-[200px] font-bold text-left text-white">
+			芳文月刊
+		</p>
+		<p className="absolute left-[158px] top-[307px] text-[70px] text-left text-white">
+			2022.12
+		</p>
+	</div>
+</div>
 	);
 };
